00069f518ee37e3b978f5cae17e8ac990c0f96e9
interal <interal@gmail.com>
2016-03-16 18:03:59 +0200

    preview card line

--- app/list.jsx
+++ app/list.jsx
@@ -1,34 +1,64 @@
 import React,{Component} from 'react'
+import {findDOMNode} from 'react-dom'
 import {connect} from 'react-redux'
 import Card from './cards.jsx'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import './styles/list.css'
+import './styles/preview-card.css'
 import cardActions from './actionCreators/cardActionCreators'
 import addCardThunk from './thunks/addCard'
 import {DropTarget} from 'react-dnd'
 import dragItemContainer from './utils/dragItemContainer'
+import _ from 'lodash'
 
 export class List extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {}
   }
 
   static dropTarget() {
+
+    function setPreviewMarker(monitor, component) {
+      let listElement = findDOMNode(component)
+      let cardsContainerEle = listElement.getElementsByClassName("cards-container")[0]
+      let cardEles = cardsContainerEle.getElementsByClassName("card")
+      let mouseY = monitor.getClientOffset().y;
+      let closestComponent = _(cardEles).minBy(element => {
+        if (!element) 
+          return Infinity
+
+        return Math.abs(element.getBoundingClientRect().bottom - mouseY)
+      })
+
+      if(closestComponent) {
+        let previewMarkerY = findDOMNode(closestComponent).getBoundingClientRect().bottom
+        component.setState({previewMarkerY}) 
+      }
+    }
+
+    function getDraggedItem(monitor) {
+        let item = monitor.getItem() || dragItemContainer.getDragItem()
+        return item
+    }
+
     return {
       canDrop(props, monitor) {
-        return true
+        let item = getDraggedItem(monitor)
+        return props.cards.indexOf(item.cardId) === -1
       },
 
       hover(props, monitor, component) {
-       
+        setPreviewMarker(monitor, component)
       },
 
       drop(props, monitor, component) {
         console.log("dropped");
-        let item = monitor.getItem() || dragItemContainer.getDragItem()
+        let item = getDraggedItem(monitor)
         let action = cardActions.moveCard(item.cardId, props.name)
         props.dispatch(action)
+        component.setState({previewMarkerY: null})
       },
     };
   }
@@ -47,29 +77,35 @@ export class List extends Component {
     this.props.dispatch(addCardThunk(this.props.name))
   }
 
+
   render() {
     let addCardBtn = <span className="add-card-btn" onClick={ this.onClickAddCardBtn.bind(this) }><b>+</b></span>
-
+    let previewDropMarker = this.props.isOver && this.state.previewMarkerY ?
+                              <hr className="preview-card-line" style={{ top: this.state.previewMarkerY + 'px' }}/> 
+                            : null
+    
     return this.props.connectDropTarget(
       <div className="list"
-           style={{ background: this.props.color}}>
+        style={{ background: this.props.color}}
+      >
 
         <h3 style={{"textAlign": "center"}}>{this.props.name}</h3>
-        
-        <ReactCSSTransitionGroup
-          transitionName="card"
-          transitionAppear={true} 
-          transitionAppearTimeout={500}
-          transitionEnterTimeout={300}
-          transitionLeaveTimeout={300}>
-                                       
-            {this.props.cards.map((c) => 
-                                        <span key={c}>
-                                           <Card cardId={c}/>
-                                          <br/>
-                                        </span>)}
-                                        
-        </ReactCSSTransitionGroup>
+        {previewDropMarker}
+        <div className="cards-container">
+          <ReactCSSTransitionGroup
+            transitionName="card"
+            transitionAppear={true} 
+            transitionAppearTimeout={500}
+            transitionEnterTimeout={300}
+            transitionLeaveTimeout={300}>
+                                         
+              {this.props.cards.map((c) => 
+                                          <span key={c}>
+                                             <Card cardId={c} />
+                                            <br/>
+                                          </span>)}
+          </ReactCSSTransitionGroup>
+        </div>                         
 
         {addCardBtn}
      </div>
